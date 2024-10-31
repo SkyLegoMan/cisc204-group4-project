832d1639c70b71059a2e00f3d9df79f5f04658f5
SY64I <158656140+SY64I@users.noreply.github.com>
2024-10-31 16:26:38 -0400

    Started trying to create propositions. Created a runable draft submission.

---
+++ example.py
@@ -0,0 +1,22 @@
+
+# TO USE THIS DRAFT: Change the following suits below to see which cards should beat what in each scenario.
+
+# Suits below can be one of the following: 'Swords', 'Cups', 'Clubs' or 'Coins'
+
+briscola_suit='Swords' # Briscola suit is the trump suit of a round. This suit should beat every other suit regardless of value
+round_suit='Cups' # 'Round Suit' is the leading suit of the round. This suit should beat any other suit, excluding the Briscola suit
+
+check_suit='Swords' # Change this suit to see which suit will beat what kind of cards in the function output.
+
+# To run the code, use python3 run.py in a terminal, after changing the values in this file.
+
+
+
+
+# Below are values that cannot change results yet (these are works in progress).
+
+# Card order is based off of order in the list.
+cards_in_play=["7 of Cups", "H of Swords", "J of Clubs", "3 of Coins"]
+
+card_to_win="7 of Cups"
+player_to_win="P2"
